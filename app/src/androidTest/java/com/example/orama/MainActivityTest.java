e9da937fe28322dbf39bb27930a4b429ca1d823a
Lucio Darcon <luciiodarcon@gmail.com>
2021-05-12 19:58:35 -0300

    import libraries for test

---
+++ app/src/androidTest/java/com/example/orama/MainActivityTest.java
@@ -0,0 +1,4 @@
+package com.example.orama;
+
+public class MainActivityTest {
+}
--- infrastructure/src/main/java/com/example/infrastructure/response/FundSituation.java
+++ infrastructure/src/main/java/com/example/infrastructure/response/FundSituation.java
@@ -1,6 +1,5 @@
 package com.example.infrastructure.response;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class FundSituation {
